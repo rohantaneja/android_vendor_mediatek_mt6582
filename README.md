ec946ce3a9a194c2bce62a76023374d6369bdab4
Rohan Taneja <rohantaneja63@gmail.com>
2016-03-15 14:25:58 +0530

    updated blobs

---
+++ README.md
@@ -0,0 +1,19 @@
+Vendor blobs for Mediatek MT6582
+---------------
+
+* Check device tree here:
+https://github.com/rohantaneja/android_device_mediatek_mt6582.git
+
+* Compilation
+
+        # repo init -u git://github.com/rohantaneja/android.git -b cm-12.1
+        
+        # repo sync
+        
+        # source build/envsetup.sh
+        
+        # brunch cm_mt6582-userdebug
+
+---------------
+
+
---
+++ mt6582-vendor.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mediatek/mt6582/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mediatek/mt6582/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mediatek/mt6582/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mediatek/mt6582/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mediatek/mt6582/proprietary/vendor,system/vendor)
